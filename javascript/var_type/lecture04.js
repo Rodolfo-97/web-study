1de8d12fd4c49032f7e25533049e1d9d7b58b245
Rodolfo-97 <sts56369223@gmail.com>
2021-04-30 15:05:18 +0900

    move file

---
+++ javascript/var_type/lecture04.js
@@ -0,0 +1,18 @@
+// 변수의 기본 자료형
+
+// Number
+var a=10, b=3.14;
+
+// String
+var c="안녕하세요", d="a";
+
+// boolean
+var e=true, f=false;
+
+
+// typeof() 명령어
+// 괄호 안의 변수의 자료형을 알려준다.
+
+console.log(a,"의 타입은",typeof(a));
+console.log(c,"의 타입은",typeof(c));
+console.log(e,"의 타입은",typeof(e));
