450e786a41254ccc64dfefc8b50fbe3cc5f1a67f
Rodolfo-97 <sts56369223@gmail.com>
2021-05-06 21:33:46 +0900

    arrow function

---
+++ javascript/function/lecture02.js
@@ -0,0 +1,8 @@
+// Arrow function
+// always anonymous function
+const simplePrint=function(){
+    console.log('simple print!!')
+};
+
+const simplePrint=()=>console.log('simple print!!');
+const add=(a, b)=>a+b;
