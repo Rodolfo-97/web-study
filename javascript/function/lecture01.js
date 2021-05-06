292625194563aae41b5ba74e4d962dfd0634a29b
Rodolfo-97 <sts56369223@gmail.com>
2021-05-06 21:33:31 +0900

    function

--- javascript/function/lecture01.js
+++ javascript/function/lecture01.js
@@ -1,4 +1,7 @@
 // function
+// 함수의 이름은 동사로
+// 하나의 함수는 한 가지 일
+// js에서 function은 object이기 때문에 변수에 할당할 수 있을 뿐만 아니라 parameter에 줄 수 있고 return값이 될 수도 있음.
 
 // 함수의 정의
 function 함수이름(인자1, 인자2){
@@ -24,8 +27,8 @@ function print(message){
     console.log(message);
     console.log("print function out")
 }
-console.log(print("Hello function"));
-// return 값이 없기 때문에 함수의 실행 결과는 undefined
+print(1234);  // 1234 출력
+print('hello')  // hello 출력
 
 // 예제 함수3
 function sum(arg1, arg2){
@@ -35,3 +38,42 @@ function sum(arg1, arg2){
 }
 console.log(sum(1, 2));
 // return 값인 3 출력
+
+// 예제 함수4 -> default 값
+function showMessage(message, from='unknown'){
+    console.log(`${message} by ${from}`)
+}
+showMessage('Hi')  // 두 번째 인자를 주지 않으면 unknown 출력
+
+// 예제함수5 -> Restparameters
+function printAll(...args){
+    for(let i=0; i<args.length; i++){
+        console.log(args[i])
+    }
+} 
+printAll(1,2,3,4,5)
+// 1,2,3,4,5 출력
+
+// Early return, early exit
+// bad
+function upgradeUser(user){
+    if(user.print>10){
+        // long logic......
+    }
+}
+
+// good
+function upgradeUser(user){
+    if(user.print<=10){
+        return;
+    }
+    // long logic......
+}
+
+// Function expression
+// Function expression 변수에 할당된 다음부터 호출이 가능하고 Function declaration은 hoist가 되기 때문에 함수가 선언되기 전에도 호출이 가능하다는 차이가 있음.
+
+const print=function(){
+    // anonymous function
+    console.log('print')
+}
